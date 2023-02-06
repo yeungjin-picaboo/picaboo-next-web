7618489cbf095955a03784e752b348bda6ee5980
woohyeon-dev <dngus3219@gmail.com>
2023-02-06 19:42:30 +0900

    fix: Correct the error that react responsive does not work when you refresh the page

--- src/components/common/Responsive.tsx
+++ src/components/common/Responsive.tsx
@@ -1,19 +1,33 @@
 import { IProps } from '@/src/types/common.interface';
+import { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 export const Responsive = ({ children }: IProps) => {
-  const isMobilePortrait = useMediaQuery({
+  const [isMobilePortrait, setIsMobilePortrait] = useState(false);
+  const [isMobileLandscape, setIsMobileLandscape] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
+  const [isPc, setIsPc] = useState(false);
+
+  const mobilePortrait = useMediaQuery({
     query: '(max-width:480px)',
   });
-  const isMobileLandscape = useMediaQuery({
+  const mobileLandscape = useMediaQuery({
     query: '(min-width:481px) and (max-width:768px)',
   });
-  const isTablet = useMediaQuery({
+  const tablet = useMediaQuery({
     query: '(min-width:769px) and (max-width:1199px)',
   });
-  const isPc = useMediaQuery({
+  const pc = useMediaQuery({
     query: '(min-width:1200px)',
   });
+
+  useEffect(() => {
+    setIsMobilePortrait(mobilePortrait);
+    setIsMobileLandscape(mobileLandscape);
+    setIsTablet(tablet);
+    setIsPc(pc);
+  }, [mobileLandscape, mobilePortrait, pc, tablet]);
+
   return (
     <>
       {isMobilePortrait && <>{children}</>}
@@ -25,29 +39,57 @@ export const Responsive = ({ children }: IProps) => {
 };
 
 export const MobilePortrait = ({ children }: IProps) => {
-  const isMobilePortrait = useMediaQuery({
+  const [isMobilePortrait, setIsMobilePortrait] = useState(false);
+
+  const mobilePortrait = useMediaQuery({
     query: '(max-width:480px)',
   });
+
+  useEffect(() => {
+    setIsMobilePortrait(mobilePortrait);
+  }, [mobilePortrait]);
+
   return <>{isMobilePortrait && children}</>;
 };
 
 export const MobileLandscape = ({ children }: IProps) => {
-  const isMobileLandscape = useMediaQuery({
+  const [isMobileLandscape, setIsMobileLandscape] = useState(false);
+
+  const mobileLandscape = useMediaQuery({
     query: '(min-width:481px) and (max-width:768px)',
   });
+
+  useEffect(() => {
+    setIsMobileLandscape(mobileLandscape);
+  }, [mobileLandscape]);
+
   return <>{isMobileLandscape && children}</>;
 };
 
 export const Tablet = ({ children }: IProps) => {
-  const isTablet = useMediaQuery({
+  const [isTablet, setIsTablet] = useState(false);
+
+  const tablet = useMediaQuery({
     query: '(min-width:769px) and (max-width:1199px)',
   });
+
+  useEffect(() => {
+    setIsTablet(tablet);
+  }, [tablet]);
+
   return <>{isTablet && children}</>;
 };
 
 export const PC = ({ children }: IProps) => {
-  const isPc = useMediaQuery({
+  const [isPc, setIsPc] = useState(false);
+
+  const pc = useMediaQuery({
     query: '(min-width:1200px)',
   });
+
+  useEffect(() => {
+    setIsPc(pc);
+  }, [pc]);
+
   return <>{isPc && children}</>;
 };
