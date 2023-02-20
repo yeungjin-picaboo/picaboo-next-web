ae9f24b1a9e4527a30d4b2c319f9be3d660da83f
woohyeon-dev <dngus3219@gmail.com>
2023-02-20 20:50:08 +0900

    refactor: Change folder structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,52 @@
-import StartLayout from '@/src/components/layout/StartLayout';
+import Link from 'next/link';
+import {
+  Layout,
+  LinkBox,
+  LinkGroup,
+  SubTitle,
+  Title,
+} from '@/src/styles/layouts/start.styles';
+import { useMediaQuery } from 'react-responsive';
+import { ArrowRight, UserPlus } from 'react-feather';
+import { Barriecito } from '@next/font/google';
 
-export default function Main() {
-  return <StartLayout />;
+const barriecito = Barriecito({ weight: '400', subsets: ['latin'] });
+
+export default function StartPage() {
+  const isTablet = useMediaQuery({
+    query: '(min-width:769px) and (max-width:1199px)',
+  });
+  const isPc = useMediaQuery({
+    query: '(min-width:1200px)',
+  });
+
+  return (
+    <Layout>
+      <Title style={barriecito.style}>
+        <span>P</span>i<span>c</span>a<span>b</span>oo draws a picture based on
+        your diary
+      </Title>
+      {(isTablet || isPc) && (
+        <SubTitle>
+          You can create an NFT
+          <br />
+          collection with diary pictures.
+        </SubTitle>
+      )}
+      <LinkGroup>
+        <LinkBox>
+          <Link href='/login' className='link'>
+            Get started
+            <ArrowRight />
+          </Link>
+        </LinkBox>
+        <LinkBox>
+          <Link href='/signup' className='link'>
+            Register
+            <UserPlus />
+          </Link>
+        </LinkBox>
+      </LinkGroup>
+    </Layout>
+  );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,78 @@
-import LoginLayout from '../components/layout/LoginLayout';
+import {
+  Layout,
+  Container,
+  Title,
+  Form,
+  Text,
+} from '@/src/styles/layouts/account.style';
+import { loginFn } from '@/src/api/accountApi';
+import useInputRef from '@/src/hooks/useInputRef';
+import { BasicBtn } from '@/src/styles/common/common.style';
+import { IsAccount } from '@/src/types/data.interface';
+import { emailOptions, passwordOptions } from '@/src/utils/inputOptions';
+import { Input, VisibilityIcon } from '@/src/components/common';
+import Link from 'next/link';
+import { AxiosError } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
+import { X } from 'react-feather';
 
-export default function Login() {
-  return <LoginLayout />;
+export default function LoginPage() {
+  const { mutate } = useMutation(loginFn, {
+    onError: (error: AxiosError) => {
+      alert(error.message);
+    },
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+    reset,
+    resetField,
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  const { currentRef: passwordCurrentRef, ...password } = useInputRef(
+    register('password', passwordOptions)
+  );
+  const onValid = async (data: IsAccount) => {
+    console.log(data);
+    mutate(data);
+    reset();
+  };
+
+  return (
+    <Layout>
+      <Container>
+        <Form onSubmit={handleSubmit(onValid)}>
+          <Title>Login</Title>
+          <Input
+            id='email'
+            label='Email'
+            placeholder='Email'
+            icon={<X onClick={() => resetField('email')} />}
+            {...register('email', emailOptions)}
+            error={errors?.email}
+          />
+          <Input
+            id='password'
+            type='password'
+            label='Password'
+            placeholder='Password'
+            icon={<VisibilityIcon _ref={passwordCurrentRef} />}
+            {...password}
+            error={errors?.password}
+          />
+          <Link href='/forgot'>Forgot password?</Link>
+          <BasicBtn disabled={isSubmitting}>Register</BasicBtn>
+        </Form>
+        <Text>
+          Don’t have an account?&nbsp;&nbsp;<Link href='/signup'>Register</Link>
+        </Text>
+      </Container>
+    </Layout>
+  );
 }
